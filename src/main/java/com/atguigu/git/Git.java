0bfc003d521b7bfa17c06e03a3e47d799f55efa5
yihongbo <yihongbo@atguigu.com>
2017-08-01 11:41:35 +0800

    ffff

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -4,5 +4,6 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("************************");
 		System.out.println("sdfsdfds");
+		System.out.println("sdfsdfds");
 	}
 }
